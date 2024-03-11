aacd0ef400a27a78c982ea4ceced619179532bf8
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-03-11 15:04:40 -0300

    Create client microservice

---
+++ msclient/msclient/src/main/java/com/douglas/msclient/MsclientApplication.java
@@ -0,0 +1,13 @@
+package com.douglas.msclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsclientApplication.class, args);
+	}
+
+}
---
+++ msclient/msclient/src/test/java/com/douglas/msclient/MsclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglas.msclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

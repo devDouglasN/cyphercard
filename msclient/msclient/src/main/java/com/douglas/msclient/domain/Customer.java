9445cbf6e1f13c512854f897dcacfe2fa3015f12
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-03-12 09:17:54 -0300

    Add JPA annotations for customer

--- msclient/msclient/src/main/java/com/douglas/msclient/domain/Customer.java
+++ msclient/msclient/src/main/java/com/douglas/msclient/domain/Customer.java
@@ -1,12 +1,19 @@
 package com.douglas.msclient.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
+@Entity
 @Data
 public class Customer {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String cpf;
     private String name;

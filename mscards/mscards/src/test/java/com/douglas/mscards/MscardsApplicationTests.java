c57382eb2508ba79384cb441ebb75679a189c9bf
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-03-15 18:37:00 -0300

    Create card microservice

---
+++ mscards/mscards/src/test/java/com/douglas/mscards/MscardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglas.mscards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

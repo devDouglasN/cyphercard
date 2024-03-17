e9ec317139a956b551223daf14782a76b236ac56
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-03-16 23:43:43 -0300

    Create CustomerCard entity

---
+++ mscards/mscards/src/main/java/com/douglas/mscards/domain/CustomerCard.java
@@ -0,0 +1,24 @@
+package com.douglas.mscards.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class CustomerCard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String cpf;
+
+    @ManyToOne
+    @JoinColumn(name = "id_card")
+    private Card card;
+
+    private BigDecimal limit;
+}

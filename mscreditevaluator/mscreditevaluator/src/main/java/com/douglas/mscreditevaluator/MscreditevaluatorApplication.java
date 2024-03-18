a2b39ae33f56e70cc9621109690029418839242e
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-03-18 10:11:39 -0300

    Create credit evaluator microservice

---
+++ mscreditevaluator/mscreditevaluator/src/main/java/com/douglas/mscreditevaluator/MscreditevaluatorApplication.java
@@ -0,0 +1,13 @@
+package com.douglas.mscreditevaluator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscreditevaluatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscreditevaluatorApplication.class, args);
+	}
+
+}
---
+++ mscreditevaluator/mscreditevaluator/src/test/java/com/douglas/mscreditevaluator/MscreditevaluatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglas.mscreditevaluator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscreditevaluatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

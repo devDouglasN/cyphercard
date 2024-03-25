3bc06d9aa7c2db8f71580a108dd34313398eb2ce
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-03-25 11:19:04 -0300

    Connecting the card microservice to RabbitMQ

--- mscards/mscards/src/main/java/com/douglas/mscards/MscardsApplication.java
+++ mscards/mscards/src/main/java/com/douglas/mscards/MscardsApplication.java
@@ -1,11 +1,13 @@
 package com.douglas.mscards;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableRabbit
 public class MscardsApplication {
 
 	public static void main(String[] args) {
